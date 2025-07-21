af292cc7972feb3262e1d5fb4fb4e04e4d6eeebe
Vrushali A Poojary <vrushaliash04@gmail.com>
2025-07-21 18:32:01 +0530

    changes in css done

--- src/components/student/CourseSection.jsx
+++ src/components/student/CourseSection.jsx
@@ -9,9 +9,9 @@ const CourseSection = () => {
   return (
     <div className='py-16 md:px-40 px-8'>
       <h2 className='text-3xl font-medium text-gray-800'>Learn from the best</h2>
-      <p className='text-sm md:text-base text-gray-500 mt-3'>Discover our top rated courses</p>
+      <p className='text-sm md:text-base text-gray-500 mt-3'>Discover our top rated <br/>courses</p> 
 
-      <div>
+      <div className='grid grid-cols-auto px-4 md:px-0 md:my-16 my-10 gap-4'>
         {allCourses.slice(0,4).map((course,index)=> <CourseCard key={index} course={course}/>)}
       </div>
 
