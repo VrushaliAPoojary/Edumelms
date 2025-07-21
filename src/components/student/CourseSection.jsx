a28232c528e6bcd1fc25740ba1a3443df925ff0c
Vrushali A Poojary <vrushaliash04@gmail.com>
2025-07-21 17:02:48 +0530

    Course section done

--- src/components/student/CourseSection.jsx
+++ src/components/student/CourseSection.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const CourseSection = () => {
   return (
-    <div>
-      <h1>Course section</h1>
+    <div className='py-16 md:px-40 px-8'>
+      <h2 className='text-3xl font-medium text-gray-800'>Learn from the best</h2>
+      <p className='text-sm md:text-base text-gray-500 mt-3'>Discover our top rated courses</p>
+
+      <Link to={'/course-list'} onClick={()=> scrollTo(0,0)} className='text-gray-500 border-gray-500/30 px-10 py-3 rounded'>Show all courses</Link>
+
+
+    
     </div>
   )
 }
