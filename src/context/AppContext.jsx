2129110aaceee0ad82963aabfbd02b44ab74ea4e
Vrushali A Poojary <vrushaliash04@gmail.com>
2025-07-24 01:36:08 +0530

    myenrollments set css pending

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -12,6 +12,7 @@ export const AppContextProvider = (props) => {
 
   const [allCourses, setAllCourses] = useState([]);
   const [isEducator, setIsEducator] = useState(true);
+  const [enrolledCourses, setEnrolledCourses] = useState([]);
 
   //fetch all courses
   const fetchAllCourses = async () => {
@@ -60,8 +61,15 @@ export const AppContextProvider = (props) => {
     return totalLectures;
   };
 
+  //fetch user enrolled courses
+
+  const fetchUserEnrolledCourses = async () => {
+    setEnrolledCourses(dummyCourses)
+  }
+
   useEffect(() => {
     fetchAllCourses();
+    fetchUserEnrolledCourses();
   }, []);
 
   const value = {
@@ -74,6 +82,8 @@ export const AppContextProvider = (props) => {
     calculateNoOfLectures,
     calculateCourseDuration,
     calculateChapterTime,
+    enrolledCourses,
+    fetchUserEnrolledCourses
   };
 
   return (
