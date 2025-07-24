7538fe3e84565ef47e8bf8e25fd74cfa21f486ac
Vrushali A Poojary <vrushaliash04@gmail.com>
2025-07-24 18:50:09 +0530

    sidebar done

--- src/pages/educator/Educator.jsx
+++ src/pages/educator/Educator.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../../components/educator/Navbar'
+import Sidebar from '../../components/educator/Sidebar'
 
 
 const Educator = () => {
   return (
     <div className='text-default min-h-screen bg-white' >
       <Navbar />
-      <div>
+      <div className='flex'>
+        <Sidebar />
+        <div className='flex-1'>
+         {<outlet/>}
+        </div>
 
-        {<outlet/>}
 
       </div>
     </div>
