902c017f635a7c5264c7a2860214f79644ea1cf0
Vrushali A Poojary <vrushaliash04@gmail.com>
2025-07-24 18:09:27 +0530

    Added navbar at educator

--- src/pages/educator/Educator.jsx
+++ src/pages/educator/Educator.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../../components/educator/Navbar'
 
 
 const Educator = () => {
   return (
-    <div>
-      <h1>educator</h1>
+    <div className='text-default min-h-screen bg-white' >
+      <Navbar />
       <div>
+
         {<outlet/>}
 
       </div>
